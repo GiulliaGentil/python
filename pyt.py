bbf2533ede9c695646e4be98c77b639b8ace3a66
GiulliaGentil <126250296+GiulliaGentil@users.noreply.github.com>
2023-05-19 11:48:04 -0300

    Add files via upload

---
+++ pyt.py
@@ -0,0 +1,27 @@
+print("Mensagem que eu quero", end="")
+
+print('Mensagem 2')
+
+print('Fulano é meu "amigo"')
+
+print("Fulano \n é meu \"amigo\"")
+
+print(1+1)
+
+print("1 x 1=",1*1)
+
+print("10³ =",10**3)
+
+print("10³ =", pow(10,3))
+
+nome = str("A")
+X = 1
+X = 1+1
+Y = True
+
+D1 = 5.5
+D2 = "5"
+print(int(D1 + int(D2)))
+print(round(D1 + int(D2)))
+
+
